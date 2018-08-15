2d2e9dbd31806fdec4963247a2c5aac12f2d954d
ByeongGil Jung <jbkcose@gmail.com>
2018-08-15 19:30:20 +0900

    [18-08-15] - Modifying incorrect words

--- spring-boot-web-mvc/src/main/java/com/example/springbootwebmvc/pizza/PizzaController.java
+++ spring-boot-web-mvc/src/main/java/com/example/springbootwebmvc/pizza/PizzaController.java
@@ -21,7 +21,7 @@ public class PizzaController {
     // HttpMessageConverter 사용
     @PostMapping("/pizzas/create")
     /*
-    public @ResponseBody Pizza create(@RequestBody Pizza home) {
+    public @ResponseBody Pizza create(@RequestBody Pizza pizza) {
 
     원래는 이 형식인데,
     @RestController 가 붙은 상황에서는,
